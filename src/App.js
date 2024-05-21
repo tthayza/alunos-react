9a36853b8c58f5b925e173509ef7c4c337a18dd2
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-05-21 13:32:09 -0300

    Start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,242 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import logoCadastro from './assets/cadastro.png'
 function App() {
+  const baseUrl = 'https://localhost:44355/api/Alunos'
+  const [data, setData] = useState([])
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  })
+  const [modalIncluir, setModalIncluir] = useState(false)
+  const [modalEditar, setModalEditar] = useState(false)
+
+  const pedidoGet = async () => {
+    await axios
+      .get(baseUrl)
+      .then((response) => {
+        setData(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  const pedidoPost = async () => {
+    delete alunoSelecionado.id
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade)
+    await axios
+      .post(baseUrl, alunoSelecionado)
+      .then((response) => {
+        setData(data.concat(response.data))
+        abrirFecharModalIncluir()
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const selecionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno)
+    opcao == 'Editar' && abrirFecharModalEditar()
+  }
+  const pedidoPut = async () => {
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade)
+    await axios
+      .put(baseUrl + '/' + alunoSelecionado.id, alunoSelecionado)
+      .then((response) => {
+        let resposta = response.data
+        let dadosAuxiliar = data
+        dadosAuxiliar.map((aluno) => {
+          if (aluno.id === alunoSelecionado.id) {
+            aluno.nome = resposta.nome
+            aluno.email = resposta.email
+            aluno.idade = resposta.idade
+          }
+        })
+        abrirFecharModalEditar()
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setAlunoSelecionado({
+      ...alunoSelecionado,
+      [name]: value
+    })
+    console.log(alunoSelecionado)
+  }
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir)
+  }
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar)
+  }
+  useEffect(() => {
+    pedidoGet()
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="aluno-container">
+      <br />
+      <header className="d-flex flex-column gap-4 align-items-center ">
+        <img src={logoCadastro} alt="Cadastro" />
+        <button
+          className="btn btn-success"
+          onClick={() => abrirFecharModalIncluir()}
         >
-          Learn React
-        </a>
+          Incluir Novo Aluno
+        </button>
       </header>
+      <table className="table table-striped table-hover">
+        <thead className="thead-dark">
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((aluno) => (
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <div className="btn-group" role="group">
+                  <button
+                    className="btn btn-primary btn-sm"
+                    onClick={() => selecionarAluno(aluno, 'Editar')}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className="btn btn-danger btn-sm ml-2"
+                    onClick={() => selecionarAluno(aluno, 'Excluir')}
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="nome"
+              onChange={handleChange}
+            />
+            <br />
+            <label>Email:</label>
+            <br />
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              onChange={handleChange}
+            />
+            <br />
+            <label>Idade:</label>
+            <br />
+            <input
+              type="number"
+              className="form-control"
+              name="idade"
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPost()}>
+            Incluir
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => abrirFecharModalIncluir()}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Editar Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID:</label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              readOnly
+              value={alunoSelecionado && alunoSelecionado.id}
+            />
+            <br />
+            <label>Nome: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="nome"
+              onChange={handleChange}
+              value={alunoSelecionado && alunoSelecionado.nome}
+            />
+            <br />
+            <label>Email:</label>
+            <br />
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              onChange={handleChange}
+              value={alunoSelecionado && alunoSelecionado.email}
+            />
+            <br />
+            <label>Idade:</label>
+            <br />
+            <input
+              type="number"
+              className="form-control"
+              name="idade"
+              onChange={handleChange}
+              value={alunoSelecionado && alunoSelecionado.idade}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPut()}>
+            Editar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => abrirFecharModalEditar()}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
